dd0231c752dc6248d41f37f674999438a55bf1d1
Patrick Nobbe <patrick.nobbe@hotmail.com>
2017-03-20 17:15:05 +0100

    Basic API interaction working

--- Netnix/app/src/main/java/com/example/patrick/netnix/MainActivity.java
+++ Netnix/app/src/main/java/com/example/patrick/netnix/MainActivity.java
@@ -1,11 +1,26 @@
 package com.example.patrick.netnix;
 
+
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,19 +33,77 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_shows:
-                    mTextMessage.setText(R.string.title_shows);
+                    makeRequest("Gotham");
                     return true;
                 case R.id.navigation_episodes:
-                    mTextMessage.setText(R.string.title_episodes);
+                    makeRequest("Suits");
                     return true;
                 case R.id.navigation_schedule:
-                    mTextMessage.setText(R.string.title_schedule);
+                    makeRequest("Game of Thrones");
                     return true;
             }
             return false;
-        }
 
+        }
     };
+
+    private void makeRequest(final String show) {
+        mTextMessage.setText("");
+        String url = "http://api.tvmaze.com/search/shows?q=" + Html.escapeHtml(show);
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray resp = new JSONArray(response);
+                            if (resp.length() > 0) {
+                                mTextMessage.setText("Search results for '" + show + "'... \n \n");
+                                for(int i = 0; i < resp.length(); i++) {
+                                    mTextMessage.append(resp.getJSONObject(i).getJSONObject("show").getString("name") + "\n");
+                                }
+                            }
+                            else {
+                                mTextMessage.setText("No shows found under '" + show + "'.");
+
+                            }
+
+                        } catch (final JSONException e) {
+                            Log.e("APIWRAPPER", "Json parsing error: " + e.getMessage());
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(getApplicationContext(),
+                                            "Json parsing error: " + e.getMessage(),
+                                            Toast.LENGTH_SHORT)
+                                            .show();
+                                }
+                            });
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("APIWRAPPER", "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "An error occurred, please try again later.",
+                                Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                });
+            }
+        });
+
+        // Add a request (in this example, called stringRequest) to your RequestQueue.
+        ApiService.getInstance(this).addToRequestQueue(stringRequest);
+    }
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
